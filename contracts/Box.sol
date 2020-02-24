f9bcedf7121179671a253dc967bb4882140f70ad
Edgar Herrador <edgar.herrador@slock.it>
2020-02-24 16:22:17 +0100

    final release

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,23 +1,4 @@
 // contracts/Box.sol
-/*pragma solidity ^0.5.0;
-
-contract Box {
-    uint256 private value;
-
-    event ValueChanged(uint256 newValue);
-
-    // Stores a new value in the contract
-    function store(uint256 newValue) public {
-        value = newValue;
-        emit ValueChanged(newValue);
-    }
-
-    // Reads the last stored value
-    function retrieve() public view returns (uint256) {
-        return value;
-    }
-}*/
-// contracts/Box.sol
 pragma solidity ^0.5.0;
 
 // Import Ownable from the OpenZeppelin Contracts library
@@ -37,5 +18,11 @@ contract Box is Ownable {
 
     function retrieve() public view returns (uint256) {
         return value;
+    }
+
+    // Increments the stored value by 1
+    function increment() public {
+        value = value + 1;
+        emit ValueChanged(value);
     }
 }
--- networks.js
+++ networks.js
@@ -1,3 +1,6 @@
+const { projectId, mnemonic } = require('./secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
@@ -8,5 +11,12 @@ module.exports = {
       gasPrice: 5e9,
       networkId: '*',
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(
+        mnemonic, `https://rinkeby.infura.io/v3/${projectId}`
+      ),
+      gasPrice: 10e9,
+      networkId: 4,
+    }
   },
 };
